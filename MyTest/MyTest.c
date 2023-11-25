6d2cfbe5d158d1c277d53474334c43e4a7263180
YanLin <chriszt@126.com>
2023-11-25 11:54:19 +0800

    完善ko模块的Demo

--- MyTest/MyTest.c
+++ MyTest/MyTest.c
@@ -1,9 +1,15 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+void Func1(void)
+{
+    printk("this is Func1()\n");
+}
+
 static int __init my_test_init(void)
 {
     int i;
+    Func1();
     printk("my first kernel module init\n");
     for (i = 0; i < 5; i++) {
         printk("init: i=%d\n", i);
@@ -12,9 +18,15 @@ static int __init my_test_init(void)
     return 0;
 }
 
+void Func2(void)
+{
+    printk("this is Func2()\n");
+}
+
 static void __exit my_test_exit(void)
 {
     int i;
+    Func2();
     printk("good bye1\n");
     for (i = 0; i < 5; i++) {
         printk("exit: i=%d\n", i);
---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+qemu-system-x86_64 -kernel /home/yl/Kernel/linux-hwe-5.15-5.15.0/arch/x86_64/boot/bzImage -initrd /home/yl/BusyBox/initrd-busybox.img -append "console=ttyS0 nokaslr" -enable-kvm -net nic,macaddr=52:54:00:12:34:56,model=e1000 -net bridge,id=net0,helper=/usr/lib/qemu/qemu-bridge-helper,br=virbr0 -nographic -S -s
