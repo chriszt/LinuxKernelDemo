b8dea70e2a6ec81e71690c1ce9d8843bbf9eff15
YanLin <chriszt@126.com>
2024-01-03 18:25:26 +0800

    增加kprobe例子

--- demo9/demo.c
+++ demo9/demo.c
@@ -1,245 +1,46 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h>
-#include <linux/slab.h>
-#include <linux/cdev.h>
-#include <linux/wait.h>
-#include <linux/kfifo.h>
-#include <linux/poll.h>
+#include <linux/kprobes.h>
 
-#define DEMO_NAME "my_demo_dev"
-#define MAX_DEMO_DEVICES 8
-#define MAX_BUFFER_SIZE 64
-#define FIFO_SIZE 64
+#define MAX_SYMBOL_LEN 64
+// static char g_symbol[MAX_SYMBOL_LEN] = "_do_fork";
+static char g_symbol[MAX_SYMBOL_LEN] = "kernel_clone";
+module_param_string(g_symbol, g_symbol, sizeof(g_symbol), 0644);
 
-struct demo_device {
-    char name[MAX_BUFFER_SIZE];
-    struct device *dev;
-    wait_queue_head_t readQueue;
-    wait_queue_head_t writeQueue;
-    struct kfifo fifo;
-};
-
-struct demo_private_data {
-    struct demo_device *demoDev;
-    char name[MAX_BUFFER_SIZE];
-};
-
-static dev_t g_dev;
-static struct cdev *g_cdev;
-static struct demo_device *g_demoDevs[MAX_DEMO_DEVICES];
-
-static int demo_open(struct inode *inode, struct file *file)
+int demo_pre_handler(struct kprobe *p, struct pt_regs *regs)
 {
-    unsigned int minor = iminor(inode);
-    struct demo_private_data *priData;
-    struct demo_device *demoDev = g_demoDevs[minor];
-    
-    printk("[demo] %s, major=%d, minor=%d, demoDev=%s\n", __func__, MAJOR(inode->i_rdev), MINOR(inode->i_rdev), demoDev->name);
-
-    priData = kmalloc(sizeof(struct demo_private_data), GFP_KERNEL);
-    if (!priData) {
-        return -ENOMEM;
-    }
-
-    sprintf(priData->name, "private_data_%d", minor);
-    priData->demoDev = demoDev;
-
-    file->private_data = priData;
-
+    pr_info("<%s> pre_handler: p->addr=0x%p, regs->ip=0x%lx, regs->flags=0x%lx\n",
+        p->symbol_name, p->addr, regs->ip, regs->flags);
     return 0;
 }
 
-static int demo_release(struct inode *inode, struct file *file)
+void demo_post_handler(struct kprobe *p, struct pt_regs *regs, unsigned long flags)
 {
-    struct demo_private_data *priData = file->private_data;
-    kfree(priData);
-    printk("[demo] %s\n", __func__);
-    return 0;
+    pr_info("<%s> post_handler: p->addr=0x%p, regs->ip=0x%lx, regs->flags=0x%lx, flags=0x%lx\n",
+        p->symbol_name, p->addr, regs->ip, regs->flags, flags);
 }
 
-static ssize_t demo_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
-{
-    struct demo_private_data *priData = file->private_data;
-    struct demo_device *demoDev = priData->demoDev;
-    int actualReaded;
-    int ret;
-
-    if (kfifo_is_empty(&demoDev->fifo)) {
-        if (file->f_flags & O_NONBLOCK) {
-            return -EAGAIN;
-        }
-        printk("[demo] %s:%s pid=%d, going to sleep, %s\n", __func__, demoDev->name, current->pid, priData->name);
-        ret = wait_event_interruptible(demoDev->readQueue, !kfifo_is_empty(&demoDev->fifo));
-        if (ret) {
-            return ret;
-        }
-    }
-
-    ret = kfifo_to_user(&demoDev->fifo, buf, count, &actualReaded);
-    if (ret) {
-        return -EIO;
-    }
-
-    if (!kfifo_is_full(&demoDev->fifo)) {
-        wake_up_interruptible(&demoDev->writeQueue);
-    }
-
-    printk("[demo] %s:%s pid=%d, actualReaded=%d, pos=%lld\n", __func__, demoDev->name, current->pid, actualReaded, *pos);
-
-    return actualReaded;
-}
-
-static ssize_t demo_write(struct file *file, const char __user *buf, size_t count, loff_t *pos)
-{
-    struct demo_private_data *priData = file->private_data;
-    struct demo_device *demoDev = priData->demoDev;
-    int actualWritten;
-    int ret;
-
-    if (kfifo_is_full(&demoDev->fifo)) {
-        if (file->f_flags & O_NONBLOCK) {
-            return -EAGAIN;
-        }
-        printk("[demo] %s:%s pid=%d, going to sleep, %s\n", __func__, demoDev->name, current->pid, priData->name);
-        ret = wait_event_interruptible(demoDev->writeQueue, !kfifo_is_full(&demoDev->fifo));
-        if (ret) {
-            return ret;
-        }
-    }
-
-    ret = kfifo_from_user(&demoDev->fifo, buf, count, &actualWritten);
-    if (ret) {
-        return -EIO;
-    }
-
-    if (!kfifo_is_empty(&demoDev->fifo)) {
-        wake_up_interruptible(&demoDev->readQueue);
-    }
-
-    printk("[demo] %s:%s pid=%d, actualWritten=%d, pos=%lld\n", __func__, demoDev->name, current->pid, actualWritten, *pos);
-
-    return actualWritten;
-}
-
-__poll_t demo_poll(struct file *file, struct poll_table_struct *wait)
-{
-    __poll_t mask = 0;
-    struct demo_private_data *priData = file->private_data;
-    struct demo_device *demoDev = priData->demoDev;
-
-    poll_wait(file, &demoDev->readQueue, wait);
-    poll_wait(file, &demoDev->writeQueue, wait);
-    
-    if (!kfifo_is_empty(&demoDev->fifo)) {
-        mask |= POLLIN | POLLRDNORM;
-    }
-    if (!kfifo_is_full(&demoDev->fifo)) {
-        mask |= POLLOUT | POLLWRNORM;
-    }
-    
-    return mask;
-}
-
-static const struct file_operations g_fileOps = {
-    .owner = THIS_MODULE,
-    .open = demo_open,
-    .release = demo_release,
-    .read = demo_read,
-    .write = demo_write,
-    .poll = demo_poll
+static struct kprobe g_kp = {
+    .symbol_name = g_symbol,
+    .pre_handler = demo_pre_handler,
+    .post_handler = demo_post_handler
 };
 
 static int __init demo_init(void)
 {
-    int ret;
-    int i;
-    struct demo_device *demoDev;
-
-    ret = alloc_chrdev_region(&g_dev, 0, MAX_DEMO_DEVICES, DEMO_NAME);
+    int ret = register_kprobe(&g_kp);
     if (ret < 0) {
-        printk("[demo] alloc_chrdev_region() failed\n");
+        pr_err("register_kprobe() failed, ret=%d\n", ret);
         return ret;
     }
-    printk("[demo] alloc_chrdev_region() succeeded\n");
-
-    g_cdev = cdev_alloc();
-    if (!g_cdev) {
-        printk("[demo] cdev_alloc() failed\n");
-        ret = -ENOMEM;
-        goto UNGISTER_CHRDEV;
-    }
-    printk("[demo] cdev_alloc() succeeded\n");
-
-    cdev_init(g_cdev, &g_fileOps);
-
-    ret = cdev_add(g_cdev, g_dev, MAX_DEMO_DEVICES);
-    if (ret < 0) {
-        printk("[demo] cdev_add() failed\n");
-        goto CDEV_FAIL;
-    }
-    printk("[demo] cdev_add() succeeded\n");
-
-    for (i = 0; i < MAX_DEMO_DEVICES; i++) {
-        demoDev = kmalloc(sizeof(struct demo_device), GFP_KERNEL);
-        if (!demoDev) {
-            ret = -ENOMEM;
-            goto FREE_DEVICE;
-        }
-        sprintf(demoDev->name, "%s%d", DEMO_NAME, i);
-        init_waitqueue_head(&demoDev->readQueue);
-        init_waitqueue_head(&demoDev->writeQueue);
-        ret = kfifo_alloc(&demoDev->fifo, FIFO_SIZE, GFP_KERNEL);
-        if (ret) {
-            ret = -ENOMEM;
-            goto FREE_KFIFO;
-        }
-        g_demoDevs[i] = demoDev;
-        printk("[demo] device name=%s\n", demoDev->name);
-    }
-
-    printk("[demo] register char device succeeded: %s\n", DEMO_NAME);
-
+    pr_info("register_kprobe() succeeded, planted kprobe at 0x%p\n", g_kp.addr);
     return 0;
-
-FREE_KFIFO:
-    for (i = 0; i < MAX_DEMO_DEVICES; i++) {
-        if (&demoDev->fifo) {
-            kfifo_free(&demoDev->fifo);
-        }
-    }
-
-FREE_DEVICE:
-    for (i = 0; i < MAX_DEMO_DEVICES; i++) {
-        if (g_demoDevs[i]) {
-            kfree(g_demoDevs[i]);
-        }
-    }
-
-CDEV_FAIL:
-    cdev_del(g_cdev);
-
-UNGISTER_CHRDEV:
-    unregister_chrdev_region(g_dev, MAX_DEMO_DEVICES);
-
-    return ret;
 }
 
 static void __exit demo_exit(void)
 {
-    int i;
-    for (i = 0; i < MAX_DEMO_DEVICES; i++) {
-        if (g_demoDevs[i]) {
-            if (&g_demoDevs[i]->fifo) {
-                kfifo_free(&g_demoDevs[i]->fifo);
-            }
-            kfree(g_demoDevs[i]);
-        }
-    }
-    cdev_del(g_cdev);
-    unregister_chrdev_region(g_dev, MAX_DEMO_DEVICES);
-    printk("[demo] unregister char device succeeded: %s\n", DEMO_NAME);
+    unregister_kprobe(&g_kp);
+    pr_info("unregister_kprobe() succeeded, planted kprobe at 0x%p\n", g_kp.addr);
 }
 
 module_init(demo_init);
--- demo9/test.c
+++ demo9/test.c
@@ -3,42 +3,10 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-#include <stdlib.h>
-#include <poll.h>
 
 int main()
 {
-    struct pollfd fds[2];
-    fds[0].fd = open("/dev/my_demo_dev0", O_RDWR);
-    fds[0].events = POLLIN;
-    fds[1].fd = open("/dev/my_demo_dev1", O_RDWR);
-    fds[1].events = POLLIN;
-
-    char buf0[64];
-    char buf1[64];
-
-    while (1) {
-        int ret = poll(fds, 2, -1);
-        if (ret == -1) {
-            return ret;
-        }
-        if (fds[0].revents & POLLIN) {
-            memset(buf0, 0, sizeof(buf0));
-            ret = read(fds[0].fd, buf0, sizeof(buf0));
-            if (ret < 0) {
-                return ret;
-            }
-            fprintf(stdout, "[demo] buf0=%s\n", buf0);
-        }
-        if (fds[1].revents & POLLIN) {
-            memset(buf1, 0, sizeof(buf1));
-            ret = read(fds[1].fd, buf1, sizeof(buf1));
-            if (ret < 0) {
-                return ret;
-            }
-            fprintf(stdout, "[demo] buf1=%s\n", buf1);
-        }
-    }
+    fprintf(stdout, "%s\n", strerror(2));
 
     return 0;
 }
